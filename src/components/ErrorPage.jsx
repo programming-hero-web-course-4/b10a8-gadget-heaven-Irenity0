644a6763f17afc3ed1e42e16975d6df9a8c62c47
Irenity <lilithkard77@gmail.com>
2024-11-03 18:50:46 +0600

    inital setup & navbar section done

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,7 @@
+const ErrorPage = () => {
+    return (
+        <h1 className="text-3xl font-bold mt-5 text-center">Error!!!</h1>
+    );
+};
+
+export default ErrorPage;
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <>
+        <h2 className="text-5xl font-bold">uwu</h2>
+        </>
+    );
+};
+
+export default Home;
